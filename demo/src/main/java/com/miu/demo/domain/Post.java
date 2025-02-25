a5177ea993505d316e9bc8d40eb4586855da1515
Gantogtokh <oganaa2472@gmail.com>
2025-02-25 13:58:17 -0600

    send demo project

---
+++ demo/src/main/java/com/miu/demo/domain/Post.java
@@ -0,0 +1,47 @@
+package com.miu.demo.domain;
+
+public class Post {
+    long id;
+    String name;
+    String content;
+    String author;
+    public Post() {}
+    public Post(long id, String name, String content, String author) {
+        this.id = id;
+        this.name = name;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
