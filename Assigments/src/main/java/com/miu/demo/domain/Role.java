6fb1e909560a998adea465ce4977a673d34965da
Gantogtokh <oganaa2472@gmail.com>
2025-03-03 21:19:42 -0600

    add xml

--- Assigments/src/main/java/com/miu/demo/domain/Role.java
+++ Assigments/src/main/java/com/miu/demo/domain/Role.java
@@ -1,4 +1,16 @@
 package com.miu.demo.domain;
 
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Data
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String role;
 }
